5362ef07cf70e0f8c2414ba129dce83ad44c8480
Jan Daniel Andersen (JAXX) <jaxx@oticon.com>
2019-03-07 12:21:00 +0100

    Added SignalR.

--- website/Startup.cs
+++ website/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using website.hubs;
 
 namespace website
 {
@@ -18,12 +19,14 @@ namespace website
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSignalR();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseStaticFiles();
             app.UseMvc();
+            app.UseSignalR(builder => builder.MapHub<MessageHub>("/message"));
         }
     }
 }
